f9b575b2f70f36827b862e7331b2d667cc0e43f2
John_IV <lorenz2@pdx.edu>
2020-03-19 19:55:21 -0700

    Add MountainCar code

--- NEATProgramming.py
+++ NEATProgramming.py
@@ -9,9 +9,23 @@ import matplotlib.pyplot as plot
 import visual
 
 path = "data/"
-MAX_FITNESS = 1000
-MAX_STEPS = 1000
-MAX_GENERATIONS = 150
+#MAX_FITNESS = 200
+MAX_STEPS = 200
+MAX_GENERATIONS = 1000
+#GAME_TO_TEST = 'CartPole-v0'
+GAME_TO_TEST = 'MountainCar-v0'
+
+def debugFunction():
+    env = gym.make(GAME_TO_TEST)
+    observation = env.reset()
+    observation, reward, done, info = env.step(env.action_space.sample())
+    print("***** Action and Observation Space for "+GAME_TO_TEST," *****")
+    print("Observation Space: ", env.observation_space)
+    print("Action space: ", env.action_space)
+    print("Reward: :", reward)
+    for steps in range(0,1000):
+        env.render()
+        observation, reward, done, info = env.step(2)
 
 def selectAction(net_output, game):
     if(game == 'CartPole-v0'):
@@ -19,13 +33,20 @@ def selectAction(net_output, game):
             action = 1
         else:
             action = 0
-
+    elif(game == 'MountainCar-v0'):
+        if(net_output[0] >= (2.0/3.0)):
+            action = 2
+        elif(net_output[0] >= (1.0/3.0) and net_output[0] < (2.0/3.0)):
+            action = 1
+        else:
+            action = 0
     return action
 
 def playAgent(winner, config, game):
     print(winner.fitness)
-    env_to_wrap = gym.make(game).env
-    env = wrappers.Monitor(env_to_wrap, "data/", force=True)
+#    env_to_wrap = gym.make(game).env
+#    env = wrappers.Monitor(env_to_wrap, "data/", force=True)
+    env = gym.make(game).env
     observation = env.reset()
     net = neat.nn.FeedForwardNetwork.create(winner, config)
     a_reward = 0
@@ -50,11 +71,12 @@ def eval_genomes(genomes, config):
     observations = []
     i = 0
     a_reward = 0
+    game = GAME_TO_TEST
 
     for _, g in genomes:
         net = neat.nn.FeedForwardNetwork.create(g, config)
         nets.append(net)                     # each env has its own net
-        envs.append(gym.make('CartPole-v0').env) # create our game environment
+        envs.append(gym.make(game).env) # create our game environment
         g.fitness = 0
         ge.append(g)                         # and its own genome
         observations.append(envs[i].reset()) # keep track of this observation
@@ -66,10 +88,7 @@ def eval_genomes(genomes, config):
         for x in range(0, len(envs)):
            if(isDone[x] == False):
                 action = nets[x].activate(observations[x])
-                if(action[0] > 0.5):
-                    action = 1
-                else:
-                    action = 0
+                action = selectAction(nets[x].activate(observations[x]), game)
                 # perform an action based on our observation, and update our observation
                 observations[x], a_reward, isDone[x], info = envs[x].step(action)
                 ge[x].fitness += a_reward  # update the reward
@@ -79,26 +98,28 @@ def eval_genomes(genomes, config):
             break
 
 def run(config_path):
-        config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
-        p = neat.Population(config)
-        p.add_reporter(neat.StdOutReporter(True))
-        stats = neat.StatisticsReporter()
-        p.add_reporter(stats)
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
 
-        pe = neat.ParallelEvaluator(4, eval_genomes)
-        winner = p.run(eval_genomes, MAX_GENERATIONS)
-        # winner = p.run(50, eval_genomes)
-        print('\nBest genome:\n{!s}'.format(winner))
+    pe = neat.ParallelEvaluator(4, eval_genomes)
+    winner = p.run(eval_genomes, MAX_GENERATIONS)  ## uncomment when not debugging
 
-        # Plot
-        visual.plot(stats, 'CartPole-v0', view=True)
-        visual.species(stats, 'CartPole-v0-generations', view=True)
+#    debugFunction() ## for sampling the observation and action space of an OpenAI Game
 
-        # Save winner
-        with open(path + 'CartPoleWinner', 'wb') as f:
-            pickle.dump(winner, f)
+    print('\nBest genome:\n{!s}'.format(winner))
 
-        playAgent(winner, config, 'CartPole-v0')
+    # Plot
+    visual.plot(stats, GAME_TO_TEST, view=True)
+    visual.species(stats, GAME_TO_TEST+'-generations', view=True)
+
+    # Save winner
+    with open(path + GAME_TO_TEST+'Winner', 'wb') as f:
+        pickle.dump(winner, f)
+
+    playAgent(winner, config, GAME_TO_TEST)
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
